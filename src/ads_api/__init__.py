d183cee9f99e0ef64fe85aca2602281b356fead6
sepnser <2387813033@qq.com>
2026-01-15 13:29:20 +0800

    1

--- src/ads_api/__init__.py
+++ src/ads_api/__init__.py
@@ -1,7 +1,14 @@
 from .setting import settings
-from .base import create_ads_client, Credentials, BaseWithProfileId, Region
+from .base import create_ads_client, Credentials, BaseWithProfileId
+from .enums import Region
 
-__all__ = ["settings", "create_ads_client", "Credentials", "BaseWithProfileId", "Region"]
+__all__ = [
+    "settings",
+    "create_ads_client",
+    "Credentials",
+    "BaseWithProfileId",
+    "Region",
+]
 
 
 def set_proxy(proxy_url: str):
