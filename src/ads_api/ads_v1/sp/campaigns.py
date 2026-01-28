9a06445f2d78248f1a5c3acf2ccb3c9b96d5f886
sepnser <2387813033@qq.com>
2026-01-29 06:21:40 +0800

    1

--- src/ads_api/ads_v1/sp/campaigns.py
+++ src/ads_api/ads_v1/sp/campaigns.py
@@ -21,6 +21,7 @@ __all__ = [
     "SPCampaignStateFilter",
     "SPCampaignCreate",
     "SPCampaignUpdate",
+    "SPCampaign"
 ]
 
 
--- src/ads_api/ads_v1/sp_global/campaigns.py
+++ src/ads_api/ads_v1/sp_global/campaigns.py
@@ -21,6 +21,7 @@ __all__ = [
     "SPGlobalCampaignUpdate",
     "CampaginGlobalApi",
     "ListGlobalCampaignFilter",
+    "SPGlobalCampaign"
 ]
 
 
